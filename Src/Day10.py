5fb3be8ac22282c0e25f55a456bdf0ff726d9698
blank26-10 <pradeepbabalad@gmail.com>
2026-02-12 14:00:43 +0530

    Day10 Task

---
+++ Src/Day10.py
@@ -0,0 +1,21 @@
+#Task 1
+import pandas as pd
+df = pd.read_csv("sample_orders.csv")
+print(df.head())
+print(df.isna().sum())
+print(df.fillna(0))
+print( df["quantity"].fillna(df["quantity"].median()))
+print(df.duplicated().sum())
+print(df.drop_duplicates())
+
+#Task 2
+print(df.dtypes)
+print(df["price"].astype(str))
+print(df["price"].str.replace("$",""))
+print(df["quantity"].astype(float))
+
+
+#Task 3
+print(df["product"].str.strip())
+print(df["product"].str.lower())
+print(df["customer_name"].unique().sum())
