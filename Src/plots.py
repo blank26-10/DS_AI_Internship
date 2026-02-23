d12b16bade89716cae89543d23e845b4024ed0ed
blank26-10 <pradeepbabalad@gmail.com>
2026-02-23 13:38:51 +0530

    edited in feature branch

--- Src/plots.py
+++ Src/plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 plt.title("Monthly Revenue Growth")
-months = [1, 2, 3, 4, 5]
-revenue = [2000, 4500, 4000, 7500, 9000]
+months = [1, 2, 3, 4, ]
+revenue = [2000, 4500, 4000, 7500]
 plt.xlabel("months")
 plt.ylabel("revenu")
 plt.plot(months,revenue)
